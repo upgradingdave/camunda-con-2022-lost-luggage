d365d96a0fa88a8b43ea44d1b0390f9c0721d66e
Brian Schlosser <bschlosser@bp-3.com>
2022-10-04 13:43:01 +0200

    Move locations into a map

--- src/main/java/org/camunda/community/lostluggage/connector/LocalizeAirtag.java
+++ src/main/java/org/camunda/community/lostluggage/connector/LocalizeAirtag.java
@@ -7,20 +7,28 @@ import org.camunda.cherry.definition.RunnerParameter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class LocalizeAirtag extends AbstractWorker {
+    private static final Map<String, List<Double>> LOCATIONS = Map.of(
+        "Statue of Liberty", Arrays.asList(40.6892494, -74.04450039999999),
+        "Newark Airport", Arrays.asList(40.6895314, -74.1744624),
+        "JFK Airport", Arrays.asList(40.6413113, -73.7781383),
+        "Muchen Airport", Arrays.asList(48.3509684, 11.7764347),
+        "Berlin Airport", Arrays.asList(52.36478700000001, 13.511414),
+        "Camunda Office", Arrays.asList(52.494709, 13.3959074)
+    );
 
     public LocalizeAirtag() {
         super("localize",
-                Arrays.asList(
-                        RunnerParameter.getInstance("airtagId", "AirtagId", String.class, RunnerParameter.Level.REQUIRED, "Airtag ID")
-                ),
-                Arrays.asList(
+                List.of(RunnerParameter.getInstance("airtagId", "AirtagId", String.class, RunnerParameter.Level.REQUIRED, "Airtag ID")),
+                List.of(
                         RunnerParameter.getInstance("lnt", "Longitude", String.class, RunnerParameter.Level.REQUIRED, "Longitude of the airtag"),
-                        RunnerParameter.getInstance("lat", "Latitude", String.class, RunnerParameter.Level.REQUIRED, "Latitude of the airtag")),
+                        RunnerParameter.getInstance("lat", "Latitude", String.class, RunnerParameter.Level.REQUIRED, "Latitude of the airtag")
+                ),
                 Collections.emptyList()
         );
-
     }
 
     @Override
@@ -33,24 +41,12 @@ public class LocalizeAirtag extends AbstractWorker {
         return "Air tag localization";
     }
 
-
     @Override
     public String getDescription() {
         return "From an Airtag ID, return the localization";
     }
 
-
     @Override
     public void execute(final JobClient jobClient, final ActivatedJob activatedJob, ContextExecution contextExecution) {
-/*
-Newark lat:40.6895314, lng:-74.1744624
-statue of liberty: lat:40.6892494, lng:-74.04450039999999
-JFK airport : lat:40.6413113, lng:-73.7781383
-muchen : lat:48.3509684, lng:11.7764347
-Berlin airport : lat:52.36478700000001, lng:13.511414
-Camunda office : lat:52.494709, lng:13.3959074
-
-
- */
     }
 }
