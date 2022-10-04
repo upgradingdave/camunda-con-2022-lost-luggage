e346aaa99bc2573047b09a67126ced206bb61aca
Brian Schlosser <bschlosser@bp-3.com>
2022-10-04 13:58:58 +0200

    Connect locations in the execute method

--- src/main/java/org/camunda/cherry/lostluggage/connector/LocalizeAirtag.java
+++ src/main/java/org/camunda/cherry/lostluggage/connector/LocalizeAirtag.java
@@ -6,28 +6,31 @@ import org.camunda.cherry.definition.AbstractWorker;
 import org.camunda.cherry.definition.RunnerParameter;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class LocalizeAirtag extends AbstractWorker {
-    private static final Map<String, List<Double>> LOCATIONS = Map.of(
-        "Statue of Liberty", Arrays.asList(40.6892494, -74.04450039999999),
-        "Newark Airport", Arrays.asList(40.6895314, -74.1744624),
-        "JFK Airport", Arrays.asList(40.6413113, -73.7781383),
-        "Muchen Airport", Arrays.asList(48.3509684, 11.7764347),
-        "Berlin Airport", Arrays.asList(52.36478700000001, 13.511414),
-        "Camunda Office", Arrays.asList(52.494709, 13.3959074)
+    private static final String AIRTAG_ID = "airtagId";
+    private static final String LATITUDE = "lat";
+    private static final String LONGITUDE = "lon";
+
+    private static final Map<String, Map<String, Object>> LOCATIONS = Map.of(
+            "Statue of Liberty", Map.of(LATITUDE, 40.6892494, LONGITUDE, -74.04450039999999),
+            "Newark Airport", Map.of(LATITUDE, 40.6895314, LONGITUDE, -74.1744624),
+            "JFK Airport", Map.of(LATITUDE, 40.6413113, LONGITUDE, -73.7781383),
+            "Muchen Airport", Map.of(LATITUDE, 48.3509684, LONGITUDE, 11.7764347),
+            "Berlin Airport", Map.of(LATITUDE, 52.36478700000001, LONGITUDE, 13.511414),
+            "Camunda Office", Map.of(LATITUDE, 52.494709, LONGITUDE, 13.3959074)
     );
 
     public LocalizeAirtag() {
         super("localize",
-                List.of(RunnerParameter.getInstance("airtagId", "AirtagId", String.class, RunnerParameter.Level.REQUIRED, "Airtag ID")),
+                List.of(RunnerParameter.getInstance(AIRTAG_ID, "AirtagId", String.class, RunnerParameter.Level.REQUIRED, "Airtag ID")),
                 List.of(
-                        RunnerParameter.getInstance("lnt", "Longitude", String.class, RunnerParameter.Level.REQUIRED, "Longitude of the airtag"),
-                        RunnerParameter.getInstance("lat", "Latitude", String.class, RunnerParameter.Level.REQUIRED, "Latitude of the airtag")
+                        RunnerParameter.getInstance(LATITUDE, "Latitude", Double.class, RunnerParameter.Level.REQUIRED, "Latitude of the airtag"),
+                        RunnerParameter.getInstance(LONGITUDE, "Longitude", Double.class, RunnerParameter.Level.REQUIRED, "Longitude of the airtag")
                 ),
                 Collections.emptyList()
         );
@@ -50,5 +53,10 @@ public class LocalizeAirtag extends AbstractWorker {
 
     @Override
     public void execute(final JobClient jobClient, final ActivatedJob activatedJob, ContextExecution contextExecution) {
+        String airtagId = getInputStringValue(AIRTAG_ID, null, activatedJob);
+
+        Map<String, Object> location = LOCATIONS.get(airtagId);
+        setValue(LATITUDE, location.get(LATITUDE), contextExecution);
+        setValue("lon", location.get("lon"), contextExecution);
     }
 }
