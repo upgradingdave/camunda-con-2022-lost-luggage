b41d869742e96af55827a28c170d9d85e1942d38
Dave Paroulek <david.paroulek@camunda.com>
2022-10-04 14:59:36 +0200

    Added unit test support

---
+++ src/test/java/org/camunda/cherry/lostluggage/connector/TestCreateImage.java
@@ -0,0 +1,14 @@
+package org.camunda.cherry.lostluggage.connector;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class TestCreateImage {
+
+  @Test
+  public void sanity() {
+    assertTrue(true);
+  }
+}
